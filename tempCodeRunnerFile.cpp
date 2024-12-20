4d894860e662767ee8017dd8f57b4b11df7a30ea
DinNamprom <56390732+DinNamprom@users.noreply.github.com>
2024-12-20 16:07:03 +0700

    lab11_2 complete

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,45 +1,95 @@
-#include <iostream>
-#include <string>
+#include<iostream>
 #include <ctime>
+#include <cstdlib>
+#include<string>
+
 using namespace std;
 
-int main() {
-    string grade;
-    cout << "Press Enter 3 times to reveal your future. \n";
-    for (int i=0;i<3;i++) {
-        cin.get();
-    }
+string cardNames[] = {"","A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+int cardScores[] = {0,1,2,3,4,5,6,7,8,9,10,10,10,10};
+
+int drawCard(void){
+	int a = rand()%13;
+	return a+1;
+}
+
+int calScore(int x,int y,int z){
+    if (x > 10)x = 10;
+    if (y > 10)y = 10;
+    if (z > 10)z = 10;
+	int score = x+y+z;
+	return score%10;
+}
+
+int findYugiAction(int s){	
+	if(s == 9) return 2; 
+	else if(s < 6) return 1;
+	else{
+		int p = rand()%100;
+		if(p < 69) {
+            return 1;
+        }else {
+            return 2;
+        }
+	}
+}
+
+void checkWinner(int p, int y){
+	if (p > y) {
+		cout << "\n---------------------------------\n"; 
+		cout <<   "|         Player wins!!!        |"; 
+		cout << "\n---------------------------------\n";
+	}else if (p < y) {
+		cout << "\n---------------------------------\n";
+		cout <<   "|          Yugi wins!!!         |"; 
+		cout << "\n---------------------------------\n";
+	}else {
+		cout << "\n---------------------------------\n";
+		cout <<   "|             Draw!!!           |";
+		cout << "\n---------------------------------\n";
+	}
+}
+
+int main(){	
     srand(time(0));
-    int o = rand()%9;
-    switch(o) {
-        case 0:
-            grade = "A";
-            break;
-        case 1:
-            grade = "B+";
-            break;
-        case 2:
-            grade = "B";
-            break;
-        case 3:
-            grade = "C+";
-            break;
-        case 4:
-            grade = "C";
-            break;
-        case 5:
-            grade = "D+";
-            break;
-        case 6:
-            grade = "D";
-            break;
-        case 7:
-            grade = "F";
-            break;
-        default:
-            grade = "w";
-            break;
-    }
-    cout << "You will get " << grade << " in this 261102.";
-    return 0;
+
+	int playerScore, yugiScore, playerAction, yugiAction;
+	int playerCards[3] = {drawCard(), drawCard(), 0};
+	int yugiCards[3] = {drawCard(), drawCard(), 0}; 
+	
+	
+	cout << "---------ORE NO TURN, DRAW!!!------------\n";
+	cout << "Your cards: " << cardNames[playerCards[0]] << " " << cardNames[playerCards[1]] << "\n";
+	playerScore = calScore(playerCards[0],playerCards[1],playerCards[2]);
+	cout << "Your score: " << playerScore << "\n";
+	do{
+		cout << "(1) Destiny draw (2) Stay, SELECT: ";
+		cin >> playerAction;
+	}while(playerAction > 2 && playerAction > 0); 
+	if(playerAction == 1){
+		playerCards[2] = drawCard();
+  		playerScore = calScore(playerCards[0],playerCards[1],playerCards[2]);
+		cout << "Player draws the 3rd card!!!" << "\n";
+		cout << "Your 3rd card: " << cardNames[playerCards[2]] << "\n";
+  		playerScore = calScore(playerCards[0],playerCards[1],playerCards[2]);
+		cout << "Your new score: " << playerScore << "\n";
+	}
+	cout << "------------ Turn end -------------------\n\n";
+	
+	cout << "---------YUGI'S TURN, DRAW!!!------------\n";
+	cout << "Yugi's cards: "<< cardNames[yugiCards[0]] << " " << cardNames[yugiCards[1]] << "\n";
+	yugiScore = calScore(yugiCards[0],yugiCards[1],yugiCards[2]);
+	cout << "Yugi's score: " << yugiScore << "\n";
+	yugiAction = findYugiAction(yugiScore);
+	if(yugiAction == 1){
+		cout << "Yugi draws the 3rd card!!!\n";
+		yugiCards[2] = drawCard();
+		cout << "Yugi's 3rd card: " << cardNames[yugiCards[2]] << "\n";
+		yugiScore = calScore(yugiCards[0],yugiCards[1],yugiCards[2]);
+		cout << "Yugi's new score: " << yugiScore << "\n";
+	}
+	cout << "------------ Turn end -------------------\n";
+
+	
+	checkWinner(playerScore,yugiScore);
 }
